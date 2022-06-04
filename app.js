da7807f952d5698ec2f0453d2262f3e0002907be
Cameron LeJeune <cameron.lejeune93@gmail.com>
2022-06-04 18:22:50 -0500

    add hash to auth

--- app.js
+++ app.js
@@ -4,7 +4,8 @@ const express = require("express");
 const bodyParser = require("body-parser");
 const ejs = require("ejs");
 const mongoose = require("mongoose");
-const encrypt = require("mongoose-encryption");
+const md5 = require('md5');
+// const encrypt = require("mongoose-encryption");
 
 const app = express();
 const port = 3000;
@@ -24,7 +25,7 @@ const userSchema = new mongoose.Schema({
   password: String
 });
 
-userSchema.plugin(encrypt, {secret: process.env.SECRET, encryptedFields: ['password'] });
+// userSchema.plugin(encrypt, {secret: process.env.SECRET, encryptedFields: ['password'] });
 
 const User = new mongoose.model ("User", userSchema);
 
@@ -43,7 +44,7 @@ app.get('/register', (req, res) => {
 app.post('/register', (req, res) => {
   let newUser = new User({
     email: req.body.username,
-    password: req.body.password
+    password: md5(req.body.password)
   });
 
   newUser.save(function(err){
@@ -57,7 +58,7 @@ app.post('/register', (req, res) => {
 
 app.post('/login', (req, res) => {
   let username = req.body.username;
-  let password = req.body.password;
+  let password = md5(req.body.password);
 
   User.findOne({email: username}, function(err, foundUser){
     if (err) {
